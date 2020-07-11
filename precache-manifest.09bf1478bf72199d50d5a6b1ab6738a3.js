9d2e02924951dd9b537d698f189597e500c5157a
leonatwork <noelabydas@gmail.com>
2020-07-11 12:19:33 +0530

    Updates

---
+++ precache-manifest.09bf1478bf72199d50d5a6b1ab6738a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "408f4b83d1bb448c1d25a3c26a76c296",
+    "url": "/cn-notes/index.html"
+  },
+  {
+    "revision": "47be77c623af8fcb1919",
+    "url": "/cn-notes/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "f72615e965177a46c169",
+    "url": "/cn-notes/static/css/main.8d7c9057.chunk.css"
+  },
+  {
+    "revision": "47be77c623af8fcb1919",
+    "url": "/cn-notes/static/js/2.1e0aa59a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/cn-notes/static/js/2.1e0aa59a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f72615e965177a46c169",
+    "url": "/cn-notes/static/js/main.c5b3d487.chunk.js"
+  },
+  {
+    "revision": "d688a06104997c3a17a2",
+    "url": "/cn-notes/static/js/runtime-main.53d47c81.js"
+  }
+]);
